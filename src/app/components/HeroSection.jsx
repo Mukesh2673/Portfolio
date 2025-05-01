399b17d20436e9cca9d18ee0b41ae2251a871504
jasdeep <development.geektech@gmail.com>
2025-05-01 10:28:23 +0530

    first commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,13 +22,11 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Judy",
+                "Mukesh",
                 1000,
                 "Web Developer",
                 1000,
-                "Mobile Developer",
-                1000,
-                "UI/UX Designer",
+                "MERN Developer",
                 1000,
               ]}
               wrapper="span"
